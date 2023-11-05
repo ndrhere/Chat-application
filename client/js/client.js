b78f3edd848e318e10943d7f430193dda8855d47
Narendra Chawda <119152426+ndrhere@users.noreply.github.com>
2023-11-06 00:11:12 +0530

    Update client.js

--- client/js/client.js
+++ client/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-application-backend-chi.vercel.app/', {transports:["websocket"]});
+const socket = io('https://chat-application-backend-chi.vercel.app', {transports:["websocket"]});
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
